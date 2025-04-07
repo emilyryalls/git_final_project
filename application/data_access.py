5b05bab1831c536589a259230707afc9f0ad7d23
Reanna Gibson <reannagibson@gmail.com>
2025-04-07 21:54:22 +0100

    EDIT: Trying to get blogs to pull from database. Still not fixed. Updated routes and data access

--- application/data_access.py
+++ application/data_access.py
@@ -1,14 +1,38 @@
 import mysql.connector
 import sys
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="password",
-  database="blog_db")
+from unicodedata import category
 
 if sys.platform == "win32":
     mysql_password = "password"
 else:
     mysql_password = ""
+
+def get_db_connection():
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="password",
+      database="blog_db")
+    return mydb
+
+def get_all_blogs():
+    # establishing a connection with the database
+    conn = get_db_connection()
+    # cursor is a method that gives ability to interact with database
+    cursor = conn.cursor()
+    sql_get_all_blogs = "SELECT title, summary, image FROM blogs"
+    # equivalent of clicking lightning bolt
+    cursor.execute(sql_get_all_blogs)
+    result = cursor.fetchall()
+    blog_list = []
+
+    for item in result:
+        blog_list.append({'title': item[0],'summary': item[1],'image': item[2]})
+    return blog_list
+
+
+
+
+
 
--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,26 @@
 from application import app
 from flask import render_template, request, redirect, url_for
 
+from application.data_access import get_all_blogs
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
 # All blogs page including filter option
-@app.route('/blog_home', methods=['GET', 'POST'])
+@app.route('/blog_home', methods=['GET'])
 def blogs():
-    return render_template('blog_home.html')
+    blog_database = get_all_blogs()
+    return render_template('blog_home.html', blog_home=blog_database)
+
+
+
+
+
+
+
 
 @app.route('/create-blog', methods=['GET', 'POST'])
 def create_blog():
