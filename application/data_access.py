16c2b9bae25314d03a9436ce8bc7c7055fd78a76
Reanna Gibson <reannagibson@gmail.com>
2025-04-08 15:45:37 +0100

    CREATE: Implemented individual blog view page
    - Created the  layout to display full blog articles.
    - Set up dynamic routing () to handle individual blog views.
    - Updated  to retrieve a single blog by its ID.
    - Clicking a blog card on  now routes to the full blog article.

--- application/data_access.py
+++ application/data_access.py
@@ -16,6 +16,7 @@ def get_db_connection():
       database="blog_db")
     return mydb
 
+# <----- Orignal get all blogs. We no longer need this ----->
 # def get_all_blogs():
 #     # establishing a connection with the database
 #     conn = get_db_connection()
@@ -32,6 +33,7 @@ def get_db_connection():
 #     return blog_list
 
 
+# <----- Get all blog function ----->
 def get_all_blogs(category=None):
     # Connect to the MySQL database
     conn = get_db_connection()
@@ -40,19 +42,45 @@ def get_all_blogs(category=None):
     # Check if a category was provided from the filter dropdown
     if category:
         # SQL query to fetch blogs that match the selected category
-        sql_query_category = "SELECT title, summary, image FROM blogs WHERE category = %s"
+        sql_query_category = "SELECT id, title, summary, image FROM blogs WHERE category = %s"
         # Execute the query with category as parameter (MUST be a tuple)
         cursor.execute(sql_query_category, (category,))
     else:
         # If no category selected, fetch all blogs
-        sql_query = "SELECT title, summary, image FROM blogs"
+        sql_query = "SELECT id, title, summary, image FROM blogs"
         cursor.execute(sql_query)
 
     # Fetch all results from the executed query
     result = cursor.fetchall()
 
     # Convert raw DB rows into a list of blog dictionaries
-    blog_list = [{'title': item[0], 'summary': item[1], 'image': item[2]} for item in result]
+    blog_list = [{'id': item[0], 'title': item[1], 'summary': item[2], 'image': item[3]} for item in result]
 
     # Return the list
     return blog_list
+
+
+# <----- Get singular blog ------>
+def get_blog_by_id(blog_id):
+    # Establish a connection to the database
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    # Execute the query to get the blog based on the blog ID
+    cursor.execute("SELECT id, title, summary, image, content FROM blogs WHERE id = %s", (blog_id,))
+
+    # Fetch the result
+    blog = cursor.fetchone()
+    # Close the connection to the database
+    conn.close()
+
+    # If no blog is found, return None
+    if blog is None:
+        return None
+    # Return the blog details as a dictionary
+    return {
+        'id': blog[0],
+        'title': blog[1],
+        'summary': blog[2],
+        'image': blog[3],
+        'content': blog[4]
+    }
--- application/routes.py
+++ application/routes.py
@@ -2,20 +2,15 @@ from application import app
 from flask import render_template, request, redirect, url_for
 
 from application.data_access import get_all_blogs
-
+from application.data_access import get_blog_by_id
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
-# # All blogs page
-# @app.route('/blog_home', methods=['GET'])
-# def blogs():
-#     blog_database = get_all_blogs()
-#     return render_template('blog_home.html', blog_list=blog_database)
 
-
+# Route to display all blogs and filter
 @app.route('/blog_home', methods=['GET'])
 def blogs():
     # Retrieve the 'category' parameter
@@ -28,6 +23,24 @@ def blogs():
     return render_template('blog_home.html', blog_list=blog_database)
 
 
+# Route to display an individual blog based on the blog ID
+@app.route('/blog/<int:blog_id>', methods=['GET'])
+def view_blog(blog_id):
+    # Call the data access function to get the blog details
+    blog = get_blog_by_id(blog_id)
+
+    if blog is None:
+        # If no blog is found, redirect to the blog home page or show a 404
+        return redirect(url_for('blogs'))
+
+    # Render the template for the individual blog, passing the blog data
+    return render_template('view_blog.html', blog=blog)
+
+
+
+
+
+# <-- We dont need this any more -->
 # @app.route('/create-blog', methods=['GET', 'POST'])
 # def create_blog():
 #     return render_template('create_blog.html')
