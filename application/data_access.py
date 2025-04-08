54bbb28caf5ab43de3c209bf84fdb267e8afce8b
Reanna Gibson <reannagibson@gmail.com>
2025-04-08 12:00:05 +0100

    EDIT: edited the footer to match the nav bar colour. Edited the blog_home route and get_all_blogs in data access to allow for filtering the blogs by catagory

--- application/data_access.py
+++ application/data_access.py
@@ -16,23 +16,43 @@ def get_db_connection():
       database="blog_db")
     return mydb
 
-def get_all_blogs():
-    # establishing a connection with the database
+# def get_all_blogs():
+#     # establishing a connection with the database
+#     conn = get_db_connection()
+#     # cursor is a method that gives ability to interact with database
+#     cursor = conn.cursor()
+#     sql_get_all_blogs = "SELECT title, summary, image FROM blogs"
+#     # equivalent of clicking lightning bolt
+#     cursor.execute(sql_get_all_blogs)
+#     result = cursor.fetchall()
+#     blog_list = []
+#
+#     for item in result:
+#         blog_list.append({'title': item[0],'summary': item[1],'image': item[2]})
+#     return blog_list
+
+
+def get_all_blogs(category=None):
+    # Connect to the MySQL database
     conn = get_db_connection()
-    # cursor is a method that gives ability to interact with database
     cursor = conn.cursor()
-    sql_get_all_blogs = "SELECT title, summary, image FROM blogs"
-    # equivalent of clicking lightning bolt
-    cursor.execute(sql_get_all_blogs)
+
+    # Check if a category was provided from the filter dropdown
+    if category:
+        # SQL query to fetch blogs that match the selected category
+        sql_query_category = "SELECT title, summary, image FROM blogs WHERE category = %s"
+        # Execute the query with category as parameter (MUST be a tuple)
+        cursor.execute(sql_query_category, (category,))
+    else:
+        # If no category selected, fetch all blogs
+        sql_query = "SELECT title, summary, image FROM blogs"
+        cursor.execute(sql_query)
+
+    # Fetch all results from the executed query
     result = cursor.fetchall()
-    blog_list = []
 
-    for item in result:
-        blog_list.append({'title': item[0],'summary': item[1],'image': item[2]})
+    # Convert raw DB rows into a list of blog dictionaries
+    blog_list = [{'title': item[0], 'summary': item[1], 'image': item[2]} for item in result]
+
+    # Return the list
     return blog_list
-
-
-
-
-
-
--- application/routes.py
+++ application/routes.py
@@ -9,19 +9,25 @@ from application.data_access import get_all_blogs
 def home():
     return render_template('home.html', title='Home')
 
-# All blogs page including filter option
+# # All blogs page
+# @app.route('/blog_home', methods=['GET'])
+# def blogs():
+#     blog_database = get_all_blogs()
+#     return render_template('blog_home.html', blog_list=blog_database)
+
+
 @app.route('/blog_home', methods=['GET'])
 def blogs():
-    blog_database = get_all_blogs()
+    # Retrieve the 'category' parameter
+    category = request.args.get('category')
+
+    # If no category is selected, it fetches all blogs
+    blog_database = get_all_blogs(category)
+
+    # Pass the list of blogs to the 'blog_home' template
     return render_template('blog_home.html', blog_list=blog_database)
 
 
-
-
-
-
-
-
-@app.route('/create-blog', methods=['GET', 'POST'])
-def create_blog():
-    return render_template('create_blog.html')
+# @app.route('/create-blog', methods=['GET', 'POST'])
+# def create_blog():
+#     return render_template('create_blog.html')
